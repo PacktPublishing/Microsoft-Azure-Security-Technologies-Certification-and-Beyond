8f5f7424dc0581d1e2bdd8d2ccbde72273b4dbe0
David Okeyode <asegunlolu@hotmail.com>
2020-12-29 15:02:08 +0000

    added user password spray scenario scripts

---
+++ chapter-2/azure-password-spray/cleanup-test-users.ps1
@@ -0,0 +1,7 @@
+﻿$upnsuffix=$(az ad signed-in-user show --query userPrincipalName --output tsv | sed 's/.*@//')
+$users = "sandra@$upnsuffix","mike@$upnsuffix","juan@$upnsuffix","kwasi@$upnsuffix","adaeze@$upnsuffix"
+
+foreach ($user in $users) 
+{
+    Remove-AzADUser -UserPrincipalName $user -Confirm:$False -Force
+}
