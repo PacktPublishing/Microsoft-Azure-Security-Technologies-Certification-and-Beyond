87adde98e0aa8c09f33c1b089fa7d0b48bcb7110
David Okeyode <asegunlolu@hotmail.com>
2021-01-01 14:44:36 +0000

    Made scripts more verbose

--- chapter-3/admin-token-theft/admin-token-theft-scenario-windows.ps1
+++ chapter-3/admin-token-theft/admin-token-theft-scenario-windows.ps1
@@ -4,13 +4,16 @@ $password = Read-Host "Please enter a password"
 $securepassword = ConvertTo-SecureString -String $password -AsPlainText -Force
 $user = "victimadminuser@$upnsuffix"
 $displayname=$(echo $user | sed 's/@.*//')
-Write-Host -ForegroundColor Green "Creating new admin user $user in Azure AD"
+Write-Host -ForegroundColor Green "#############################################"
+Write-Host -ForegroundColor Green "# Creating new admin user $user in Azure AD #"
+Write-Host -ForegroundColor Green "#############################################"
 New-AzADUser -DisplayName $displayname -UserPrincipalName $user -Password $securepassword -MailNickname $displayname
-
 
 ## Assign role in Azure subscription
 $subid=$(az account show --query id --output tsv)
-Write-Host -ForegroundColor Green "Assigning the Contributor role to $user"
+Write-Host -ForegroundColor Green "############################################"
+Write-Host -ForegroundColor Green "# Assigning the Contributor role to $user #"
+Write-Host -ForegroundColor Green "############################################"
 az role assignment create --role "Contributor" --assignee $user --subscription $subid
 
 ## Create Storage account with SAS token
@@ -23,7 +26,9 @@ $containername = "exfil"
 $blobname = "azureprofile.zip"
 Write-Host -ForegroundColor Green "Creating a new storage account $storagename"
 az storage account create --name $storagename --resource-group $group --location $location --sku Standard_LRS --allow-blob-public-access false --https-only true
-Write-Host -ForegroundColor Green "Creating a new blob container $containername in $storagename"
+Write-Host -ForegroundColor Green "##################################################################"
+Write-Host -ForegroundColor Green "# Creating a new blob container $containername in $storagename #"
+Write-Host -ForegroundColor Green "##################################################################"
 az storage container create --account-name $storagename --name $containername
 $ctx = New-AzStorageContext -StorageAccountName $storagename -UseConnectedAccount
 $StartTime = Get-Date
@@ -36,13 +41,18 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/PacktPublishing/Impleme
 ## Deploy Windows VM with Azure PowerShell installed (Output public IP)
 $winvmname = "winvm$random"
 $windowsuser = "windowsadmin"
-Write-Host -ForegroundColor Green "Creating a new Windows VM $winvmname"
-az vm create --resource-group $group --name $winvmname --image win2016datacenter --admin-username $windowsuser --admin-password $password --size Standard_DS2_v3
+Write-Host -ForegroundColor Green "########################################"
+Write-Host -ForegroundColor Green "# Creating a new Windows VM $winvmname #"
+Write-Host -ForegroundColor Green "########################################"
+az vm create --resource-group $group --name $winvmname --image win2016datacenter --admin-username $windowsuser --admin-password $password --size Standard_D2s_v3
 az vm open-port --port 3389 --resource-group $group --name $winvmname --priority 200
 $winvmpubip=$(az vm show -d -g $group -n $winvmname --query publicIps -o tsv)
 az vm extension set -g $group --vm-name $winvmname --name customScript --publisher Microsoft.Azure.Extensions --settings ./windows_custom_extension.json
 
 ## Script Output
+Write-Host -ForegroundColor Green "#################"
+Write-Host -ForegroundColor Green "# Script Output #"
+Write-Host -ForegroundColor Green "#################"
 Write-Host -ForegroundColor Green "Azure Admin User:" $user
 Write-Host -ForegroundColor Green "Azure Admin User Password:" $password
 Write-Host -ForegroundColor Green " "
