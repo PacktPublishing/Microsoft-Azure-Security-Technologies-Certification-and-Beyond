71042b41145743bf6a6d01abdec4bc5527e1e914
David Okeyode <asegunlolu@hotmail.com>
2021-01-01 15:09:16 +0000

    fixed powershell extension path bug

--- chapter-3/admin-token-theft/admin-token-theft-scenario-windows.ps1
+++ chapter-3/admin-token-theft/admin-token-theft-scenario-windows.ps1
@@ -4,16 +4,16 @@ $password = Read-Host "Please enter a password"
 $securepassword = ConvertTo-SecureString -String $password -AsPlainText -Force
 $user = "victimadminuser@$upnsuffix"
 $displayname=$(echo $user | sed 's/@.*//')
-Write-Host -ForegroundColor Green "#############################################"
+Write-Host -ForegroundColor Green "###########################################################################"
 Write-Host -ForegroundColor Green "# Creating new admin user $user in Azure AD #"
-Write-Host -ForegroundColor Green "#############################################"
+Write-Host -ForegroundColor Green "###########################################################################"
 New-AzADUser -DisplayName $displayname -UserPrincipalName $user -Password $securepassword -MailNickname $displayname
 
 ## Assign role in Azure subscription
 $subid=$(az account show --query id --output tsv)
-Write-Host -ForegroundColor Green "############################################"
+Write-Host -ForegroundColor Green "#########################################################################"
 Write-Host -ForegroundColor Green "# Assigning the Contributor role to $user #"
-Write-Host -ForegroundColor Green "############################################"
+Write-Host -ForegroundColor Green "#########################################################################"
 az role assignment create --role "Contributor" --assignee $user --subscription $subid
 
 ## Create Storage account with SAS token
@@ -24,11 +24,13 @@ $random = Get-Random -Maximum 10000
 $storagename = "pentest$random"
 $containername = "exfil"
 $blobname = "azureprofile.zip"
-Write-Host -ForegroundColor Green "Creating a new storage account $storagename"
+Write-Host -ForegroundColor Green "###############################################"
+Write-Host -ForegroundColor Green "# Creating a new storage account $storagename #"
+Write-Host -ForegroundColor Green "###############################################"
 az storage account create --name $storagename --resource-group $group --location $location --sku Standard_LRS --allow-blob-public-access false --https-only true
-Write-Host -ForegroundColor Green "##################################################################"
+Write-Host -ForegroundColor Green "######################################################"
 Write-Host -ForegroundColor Green "# Creating a new blob container $containername in $storagename #"
-Write-Host -ForegroundColor Green "##################################################################"
+Write-Host -ForegroundColor Green "######################################################"
 az storage container create --account-name $storagename --name $containername
 $ctx = New-AzStorageContext -StorageAccountName $storagename -UseConnectedAccount
 $StartTime = Get-Date
@@ -47,6 +49,9 @@ Write-Host -ForegroundColor Green "########################################"
 az vm create --resource-group $group --name $winvmname --image win2016datacenter --admin-username $windowsuser --admin-password $password --size Standard_D2s_v3
 az vm open-port --port 3389 --resource-group $group --name $winvmname --priority 200
 $winvmpubip=$(az vm show -d -g $group -n $winvmname --query publicIps -o tsv)
+
+Set-AzVMCustomScriptExtension -ResourceGroupName $group -VMName $winvmname -Location $location -FileUri "https://raw.githubusercontent.com/PacktPublishing/Implementing-Microsoft-Azure-Security-Technologies/main/chapter-3/custom-script-extensions/azure_powershell_install.ps1" -Run 'azure_powershell_install.ps1' -Name AzurePSExtension
+
 az vm extension set -g $group --vm-name $winvmname --name customScript --publisher Microsoft.Azure.Extensions --settings ./windows_custom_extension.json
 
 ## Script Output
